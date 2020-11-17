bb79aa1f7997d75dcb4ed17f0c5e6ad4037e09e3
NataliaRajska <natalia.rajskaa@gmail.com>
2020-11-17 10:26:54 +0100

    Add getters and setters

--- app.js
+++ app.js
@@ -3,5 +3,23 @@ const menu = {
         appetizer:[],
         mains:[],
         desserts: []
+    },
+    get appetizers(){
+        return this._courses.appetizer;
+    },
+    get mains(){
+        return this._courses.mains;
+    },
+    get desserts(){
+        return this._courses.desserts;
+    },
+    set appetizers(appetizer){
+        return this._courses.appetizer = appetizer;
+    },
+    set mains(main){
+        return this._courses.mains = main;
+    },
+    set desserts(dessert){
+        return this._courses.desserts = dessert;
     }
 };
